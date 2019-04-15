3b62f0736c804683ade30e3abf362e1a91856fb4
ksd101jp14 <46618187+kd101jp14@users.noreply.github.com>
2019-04-15 18:11:40 -0500

    Created database and JS file.

---
+++ bamazon.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS bamazon;
+CREATE DATABASE bamazon;
+USE bamazon;
+CREATE TABLE products (
+item_id INTEGER AUTO_INCREMENT NOT NULL,
+product_name VARCHAR(50) NOT NULL,
+department_name VARCHAR(50) NOT NULL,
+price DECIMAL(6,2) NOT NULL,
+stock_quantity INTEGER NOT NULL,
+PRIMARY KEY (item_id)
+);
