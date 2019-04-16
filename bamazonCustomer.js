0bc21c46946f7b7c7702dd84d875860c8b51eff4
ksd101jp14 <46618187+kd101jp14@users.noreply.github.com>
2019-04-15 20:46:42 -0500

    Fixed connection to database and 'start' function.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,13 +1,28 @@
 // Set up dependencies
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var password = require("./password_module.js");
 
 // Create connection to the sql database
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "bamazon"
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: password.password,
+    database: "bamazon"
 });
 
+connection.connect(function (err) {
+    if (err) throw err;
+    start();
+});
+
+function start() {
+    console.log("\n WELCOME TO THE INVENTORY: \n");
+    var query = "SELECT * FROM products";
+    connection.query(query, function (err, res) {
+        for (var i = 0; i < res.length; i++) {
+            console.log("\n   " + res[i].product_name + "\n");
+        }
+    });
+}
