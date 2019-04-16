d9433bd1bbc72092db19f2afe978d5f09136061e
ksd101jp14 <46618187+kd101jp14@users.noreply.github.com>
2019-04-16 08:10:28 -0500

    Displayed items on for user and provided the option to shop or exit.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,28 +1,132 @@
 // Set up dependencies
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var password = require("./password_module.js");
+var passwordModule = require("./password_module.js");
 
 // Create connection to the sql database
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: password.password,
+    password: passwordModule.password,
     database: "bamazon"
 });
 
 connection.connect(function (err) {
     if (err) throw err;
-    start();
+    welcome();
 });
 
-function start() {
-    console.log("\n WELCOME TO THE INVENTORY: \n");
+function welcome() {
+    console.log("\n *** WELCOME TO BAMAZON! ***");
+    console.log("\n * Check out our products. * \n\n");
+    var products = [];
+    var productInfo = "";
     var query = "SELECT * FROM products";
     connection.query(query, function (err, res) {
         for (var i = 0; i < res.length; i++) {
-            console.log("\n   " + res[i].product_name + "\n");
+            productInfo = "    id: " + res[i].item_id + " || item: " + res[i].product_name + " || price: $" + res[i].price;
+            products.push(productInfo);
+            console.log(productInfo + "\n");
         }
+        mainMenu();
     });
 }
+
+function mainMenu() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "SHOP",
+                "EXIT"
+            ]
+        })
+        .then(function (answer) {
+            switch (answer.action) {
+                case "SHOP":
+                    shopping();
+                    break;
+
+                case "EXIT":
+                    console.log("\n Thank you for visiting Bamazon! \n")
+                    connection.end();
+                    break;
+            }
+        });
+}
+
+function shopping() {
+    inquirer
+        .prompt({
+            name: "items",
+            type: "input",
+            message: "Which item would you like to purchase?",
+            validate: function (value) {
+                if (isNaN(value) === false && value >= 1 && value <= 10) {
+                    return true;
+                }
+                return false;
+            }
+        })
+        .then(function (answer) {
+            var query = "SELECT * FROM products";
+            connection.query(query, [answer.items], function (err, res) {
+                console.log(" \n You have selected Item: " + answer.items + ". \n");
+                inquirer
+                    .prompt({
+                        name: "quantity",
+                        type: "input",
+                        message: "How many of Item: " + answer.items + " would you like to purchase?",
+                        validate: function (value) {
+                            if (isNaN(value) === false && value >= 1) {
+                                return true;
+                            }
+                            return false;
+                        }
+                    })
+                    .then(function (answer) {
+                        var query = "SELECT * FROM products";
+                        connection.query(query, [answer.items], function (err, res) {
+                            console.log("\n You would like to buy a quantity of " + answer.quantity + ". \n");
+
+                        });
+                    });
+
+            });
+        });
+};
+//             .then(function (answer) {
+//                 switch (answer) {
+//                     case 1:
+//                         console.log("hello");
+//                         break;
+//                     case 2:
+//                         console.log("hello");
+//                         break;
+//                     case 3:
+//                         console.log("hello");
+//                         break;
+//                     case 4:
+//                         console.log("hello");
+//                         break;
+//                     case 5:
+//                         console.log("hello");
+//                         break;
+//                     case 6:
+//                         console.log("hello");
+//                         break;
+//                     case 7:
+//                         console.log("hello");
+//                         break;
+//                     case 8:
+//                         console.log("hello");
+//                         break;
+//                     case 9:
+//                         console.log("hello");
+//                         break;
+//                     case 10:
+//                         console.log("hello");
+//                         break;
